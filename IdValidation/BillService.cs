a4562a9a668cf2c1c6385b73090a1677ec09ed6e
Tannaz <tannaz.fathi@yahoo.com>
2024-05-11 21:46:56 +0430

    some changes in BillValidation cleanning code

--- IdValidation/BillService.cs
+++ IdValidation/BillService.cs
@@ -2,32 +2,27 @@
 {
     private bool ValidateBillId(string billId)
     {
-
-        bool result = true;
-
         //check length & the 0's on the left
-        char firstDigit = billId[0];
 
-        if (billId.Length > 13 || firstDigit == '0')
-        {
+        if (billId.Length < 6 || billId.Length > 13 )       
             return false;
-        }
+        
 
         result = ValidateCheckDigit(billId);
+        firstChkDigit = ValidateCheckDigit(billId.Substring(0 , billId.length-1));
 
-        return result;
+        //return result==true && firstChkDigit==true;
+        return result && firstChkDigit;
+       
+
     }
 
     private bool ValidatePaymentId(string paymentId, string billId)
     {
-
-        bool result = true;
-
         //check length
         if (paymentId.Length < 6 || paymentId.Length > 13)
-        {
             return false;
-        }
+        
 
         result = ValidateCheckDigit(billId + paymentId);
 
@@ -37,8 +32,6 @@
 
     public bool ValidateCheckDigit(string variable)
     {
-
-        bool result = true;
 
         //check checkDigit
         int checkDigit = Convert.ToInt32(variable.Substring(variable.Length - 1, 1));
@@ -53,11 +46,10 @@
         {
             sum += Convert.ToInt32(reversedText[i].ToString()) * mulNumber;
 
-            if (mulNumber == 7)
-            {
+            if (mulNumber == 7)           
                 mulNumber = 2;
                 continue;
-            }
+            
             mulNumber++;
         }
 
@@ -65,10 +57,9 @@
         int res = 0;
 
         if (residual > 1)
-        {
             res = Math.Abs(residual - 11);
             result = res.Equals(checkDigit);
-        }
+
         else
         {
             result = res.Equals(checkDigit);
@@ -80,14 +71,9 @@
 
     public bool Validate(Bill bill)
     {
-
-        bool result = false;
-
         if (ValidateBillId(bill.BillId) == true && ValidatePaymentId(bill.PaymentId, bill.BillId) == true)
-
-        {
             result = true;
-        }
+        
         return result;
     }
 
--- IdValidation/Program.cs
+++ IdValidation/Program.cs
@@ -4,24 +4,28 @@
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("start");
+            do
+            {
+                Console.WriteLine("start");
 
-            Bill billAp = new Bill();
+                Bill billAp = new Bill();
 
-            Console.WriteLine("ENTER BILLID :");
-            billAp.BillId = Console.ReadLine();
-
-
-            Console.WriteLine("ENTER PAYMENTID :");
-            billAp.PaymentId = Console.ReadLine();
-
-            BillService service = new BillService();
-
-            bool isvalid = service.Validate(billAp);
-            Console.WriteLine($" billId or paymentid is {isvalid} ");
+                Console.WriteLine("ENTER BILLID :");
+                billAp.BillId = Console.ReadLine();
 
 
-            Console.WriteLine("end");
+                Console.WriteLine("ENTER PAYMENTID :");
+                billAp.PaymentId = Console.ReadLine();
+
+                BillService service = new BillService();
+
+                bool isvalid = service.Validate(billAp);
+                Console.WriteLine($" billId or paymentid is {isvalid} ");
+
+
+                Console.WriteLine("end");
+            }
+            while(isvalid=false);
         }
     }
 }
